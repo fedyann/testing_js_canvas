d4ecf07557e0c1b99c2021191ea48fc1a4564fb8
fedyann <117765096+fedyann@users.noreply.github.com>
2024-04-24 21:56:52 +0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,71 +1,157 @@
-window.onload = function() {
+// Variables for referencing the canvas and 2dcanvas context
+var canvas,ctx;
 
-    const canvas1 = document.getElementById('myCanvas');
-    const ctx = canvas1.getContext('2d');
-    ctx.globalAlpha = 0;  // Теперь краски нет!
+// Variables to keep track of the mouse position and left-button status
+var mouseX,mouseY,moving, mouseDown=0;
 
-    document.ontouchmove = function(e){ e.preventDefault(); }
-  
-    var canvas  = document.getElementById('main');
-    var canvastop = canvas.offsetTop
-  
-    var context = canvas.getContext("2d");
-  
-    var lastx;
-    var lasty;
-  
-    context.strokeStyle = "#000000";
-    context.lineCap = 'round';
-    context.lineJoin = 'round';
-    context.lineWidth = 5;
-  
-    function clear() {
-      context.fillStyle = "#ffffff";
-      context.rect(0, 0, 300, 300);
-      context.fill();
+// Variables to keep track of the touch position
+var touchX,touchY;
+
+var userDrawnPixels = [];
+
+// Get the touch position relative to the top-left of the canvas
+// When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
+// but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
+// "target.offsetTop" to get the correct values in relation to the top left of the canvas.
+function getTouchPos(e) {
+    if (!e)
+        var e = event;
+
+    if(e.touches) {
+        if (e.touches.length == 1) { // Only deal with one finger
+            var touch = e.touches[0]; // Get the information for finger #1
+            touchX=touch.pageX-touch.target.offsetLeft;
+            touchY=touch.pageY-touch.target.offsetTop;
+        }
     }
-  
-    function dot(x,y) {
-      context.beginPath();
-      context.fillStyle = "#000000";
-      context.arc(x,y,1,0,Math.PI*2,true);
-      context.fill();
-      context.stroke();
-      context.closePath();
+}
+
+// Set-up the canvas and add our event handlers after the page has loaded
+function init() {
+    // Get the specific canvas element from the HTML document
+    canvas = document.getElementById('canvas');
+
+    canvas.focus();
+
+    canvas.width  = (window.innerWidth > 400) ? 400 : window.innerWidth;
+    canvas.height = (window.innerHeight > 400) ? 400 : window.innerHeight;
+
+    // If the browser supports the canvas tag, get the 2d drawing context for this canvas
+    if (canvas.getContext)
+        ctx = canvas.getContext('2d');
+
+    // Check that we have a valid context to draw on/with before adding event handlers
+    if (ctx) {
+        // React to mouse events on the canvas, and mouseup on the entire document
+        canvas.addEventListener('mousedown', sketchpad_mouseDown, false);
+        canvas.addEventListener('mousemove', sketchpad_mouseMove, false);
+        canvas.addEventListener('mouseup', mouseOrTouchUp, false);
+
+        // React to touch events on the canvas
+        canvas.addEventListener('touchstart', sketchpad_touchStart, false);
+        canvas.addEventListener('touchmove', sketchpad_touchMove, false);
+        canvas.addEventListener('touchend', mouseOrTouchUp, false);
     }
-  
-    function line(fromx,fromy, tox,toy) {
-      context.beginPath();
-      context.moveTo(fromx, fromy);
-      context.lineTo(tox, toy);
-      context.stroke();
-      context.closePath();
+}
+
+// Draws a dot at a specific position on the supplied canvas name
+// Parameters are: A canvas context, the x position, the y position, the size of the dot
+function drawLine(ctx, x, y, size) {
+    ctx.fillStyle = "lightgrey";
+    ctx.beginPath();
+
+    var n = userDrawnPixels.length;
+    var point = userDrawnPixels[n-1];
+
+    if ((n>1) && moving) {
+        var prevPoint = userDrawnPixels[n-2];
+        ctx.moveTo(prevPoint[0],prevPoint[1]);
+        ctx.lineTo(point[0], point[1]);
+    } else {
+        //ctx.moveTo(point[0],point[0]);
+        //ctx.lineTo(point[0], point[1]);
     }
-  
-    canvas.ontouchstart = function(event){                   
-      event.preventDefault();                 
-      
-      lastx = event.touches[0].clientX;
-      lasty = event.touches[0].clientY - canvastop;
-  
-      dot(lastx,lasty);
+
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = size;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.fill();
+}
+
+
+function drawDot(ctx, x, y, size) {
+    ctx.fillStyle = "lightgrey";
+
+    // Draw a filled circle
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+    // Keep track of the mouse button being pressed and draw a dot at current location
+function sketchpad_mouseDown() {
+
+    userDrawnPixels.push([mouseX, mouseY]);
+    drawDot(ctx,mouseX,mouseY,3);
+
+    mouseDown=1;
+}
+
+
+function mouseOrTouchUp() {
+    mouseDown=0;
+    moving=0;
+}
+
+function sketchpad_mouseMove(e) {
+    // Update the mouse co-ordinates when moved
+    getMousePos(e);
+
+    // Draw a dot if the mouse button is currently being pressed
+    if (mouseDown==1) {
+        drawLine(ctx,mouseX,mouseY,6);
+        userDrawnPixels.push([mouseX, mouseY]);
+        moving=1;
     }
-  
-    canvas.ontouchmove = function(event){                   
-      event.preventDefault();                 
-  
-      var newx = event.touches[0].clientX;
-      var newy = event.touches[0].clientY - canvastop;
-  
-      line(lastx,lasty, newx,newy);
-      
-      lastx = newx;
-      lasty = newy;
+}
+
+// Get the current mouse position relative to the top-left of the canvas
+function getMousePos(e) {
+    if (!e)
+        var e = event;
+
+    if (e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
     }
-  
-  
-    var clearButton = document.getElementById('clear')
-    clearButton.onclick = clear
-  
-    clear()
-  }
+    else if (e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+}
+
+// Draw something when a touch start is detected
+function sketchpad_touchStart() {
+    getTouchPos();
+    userDrawnPixels.push([touchX, touchY]);
+    drawDot(ctx,touchX,touchY,3);
+
+    // Prevent a scrolling action as a result of this touchmove triggering.
+    event.preventDefault();
+
+    moving=1;
+}
+
+function sketchpad_touchMove(e) {
+    getTouchPos(e);
+    userDrawnPixels.push([touchX, touchY]);
+    drawLine(ctx,touchX,touchY,6);
+
+    // Prevent a scrolling action as a result of this touchmove triggering.
+    event.preventDefault();
+}
+
+init();
